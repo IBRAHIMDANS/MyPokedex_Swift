d0108cadd2779d7826438106ea7190647c566b7a
Ibrahima Dansoko <ibrahima.dansoko@outlook.com>
2019-04-16 13:59:23 +0200

    Pokedex v2

---
+++ My Pokedex/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  My Pokedex
+//
+//  Created by Ibrahima DANSOKO on 16/04/2019.
+//  Copyright © 2018 TYP Studio. All rights reserved.
+//
+
+import UIKit
+
+public struct Constants {
+    public struct api {
+        public static let base = "http://pokeapi.co/api/v2/"
+        public static let pokedex = "\(api.base)pokedex/2"
+    }
+}
